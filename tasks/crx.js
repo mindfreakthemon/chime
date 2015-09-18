7716422a39be4d7c6655d9a6b45f448ba2d7e47c
Vitalii Rakovtsii <vitalii.rakovtsii@playtech.com>
2015-09-18 13:10:52 +0300

    Removing key loading from the module ini

--- tasks/crx.js
+++ tasks/crx.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp'),
 	ChromeExtension = require('crx'),
-	fs = require('fs'),
-	crx = new ChromeExtension({
+	fs = require('fs');
+
+gulp.task('crx', ['zip'], function () {
+	var crx = new ChromeExtension({
 		privateKey: fs.readFileSync('extension/key.pem')
 	});
 
-gulp.task('crx', ['zip'], function () {
 	crx.load('extension')
 		.then(function () {
 			return crx.pack().then(function (crxBuffer) {
