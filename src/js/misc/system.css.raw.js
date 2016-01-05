2601f2f9bc087c98f65289cfd7f97bf5b1f11d4c
Vitalii Rakovtsii <vitalii.rakovtsii@playtech.com>
2016-01-05 23:40:48 +0200

    fixing formatting

--- src/js/misc/system.css.raw.js
+++ src/js/misc/system.css.raw.js
@@ -11,8 +11,8 @@ if (typeof window !== 'undefined') {
 	}
 
 	var isWebkit = !!window.navigator.userAgent.match(/AppleWebKit\/([^ ;]*)/);
-	var webkitLoadCheck = function(link, callback) {
-		setTimeout(function() {
+	var webkitLoadCheck = function (link, callback) {
+		setTimeout(function () {
 			for (var i = 0; i < document.styleSheets.length; i++) {
 				var sheet = document.styleSheets[i];
 				if (sheet.href == link.href)
@@ -22,17 +22,18 @@ if (typeof window !== 'undefined') {
 		}, 10);
 	};
 
-	var noop = function() {};
+	var noop = function () {
+	};
 
-	var loadCSS = function(url) {
-		return new Promise(function(resolve, reject) {
-			var timeout = setTimeout(function() {
+	var loadCSS = function (url) {
+		return new Promise(function (resolve, reject) {
+			var timeout = setTimeout(function () {
 				reject('Unable to load CSS');
 			}, waitSeconds * 1000);
-			var _callback = function(error) {
+			var _callback = function (error) {
 				clearTimeout(timeout);
 				link.onload = link.onerror = noop;
-				setTimeout(function() {
+				setTimeout(function () {
 					if (error)
 						reject(error);
 					else
@@ -44,55 +45,24 @@ if (typeof window !== 'undefined') {
 			link.rel = 'stylesheet';
 			link.href = url;
 			if (!isWebkit) {
-				link.onload = function() {
+				link.onload = function () {
 					_callback();
-				}
+				};
 			} else {
 				webkitLoadCheck(link, _callback);
 			}
-			link.onerror = function(event) {
+			link.onerror = function (event) {
 				_callback(event.error || new Error('Error loading CSS file.'));
 			};
 			head.appendChild(link);
 		});
 	};
 
-	exports.fetch = function(load) {
+	exports.fetch = function (load) {
 		// dont reload styles loaded in the head
 		for (var i = 0; i < linkHrefs.length; i++)
 			if (load.address == linkHrefs[i])
 				return '';
 		return loadCSS(load.address);
-	};
-}
-else {
-	function getBuilder(loader) {
-		return loader['import']('./css-builder' + (System.version ? '.js' : ''), { name: module.id });
-	}
-
-	exports.cssPlugin = true;
-	exports.fetch = function(load) {
-		// individually mark loads as not built for buildCSS false
-		if (this.buildCSS === false)
-			load.metadata.build = false;
-		// setting format = 'defined' means we're managing our own output
-		load.metadata.format = 'defined';
-		// don't load the CSS at all until build time
-		return '';
-	};
-	exports.instantiate = function() {};
-	exports.bundle = function(loads, opts) {
-		var loader = this;
-		if (loader.buildCSS === false)
-			return '';
-		return getBuilder(loader).then(function(builder) {
-			return builder.bundle.call(loader, loads, opts);
-		});
-	};
-	exports.listAssets = function(loads, compileOpts, outputOpts) {
-		var loader = this;
-		return getBuilder(loader).then(function(builder) {
-			return builder.listAssets.call(loader, loads, compileOpts, outputOpts);
-		});
 	};
 }
